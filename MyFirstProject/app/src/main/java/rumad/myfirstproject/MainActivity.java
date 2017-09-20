25af866ada981da8c16bdc006e38cc8c19ce516d
Revanth <revkrock@gmail.com>
2017-09-19 22:20:26 -0400

    init

---
+++ MyFirstProject/app/src/main/java/rumad/myfirstproject/MainActivity.java
@@ -0,0 +1,24 @@
+package rumad.myfirstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView text = (TextView) findViewById(R.id.textView);
+        Button button = (Button) findViewById(R.id.button);
+
+        text.setText("Good Bye");
+
+
+    }
+}
