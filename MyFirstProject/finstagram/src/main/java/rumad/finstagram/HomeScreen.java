f49144915fef1e27ba27e4ab36af929fd4c8c1db
Revanth <revkrock@gmail.com>
2017-10-25 19:41:27 -0400

    Week4

---
+++ MyFirstProject/finstagram/src/main/java/rumad/finstagram/HomeScreen.java
@@ -0,0 +1,111 @@
+package rumad.finstagram;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeScreen extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    //private OnFragmentInteractionListener mListener;
+
+    public HomeScreen() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static HomeScreen newInstance(String param1, String param2) {
+        HomeScreen fragment = new HomeScreen();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.fragment_home_screen,container, false);
+
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recyclerView);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] CEOs = new String[]{"Musk", "Zuck", "Nadella", "Pichai", "Cook"};
+        int[] pictures=new int[]{R.drawable.elon_musk,R.drawable.mark_zuckerberg,R.drawable.satya_nadella,R.drawable.sundar_pichai,R.drawable.tim_cook};
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(CEOs,pictures);
+        mRecyclerView.setAdapter(mAdapter);
+/*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        */
+
+
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
--- MyFirstProject/finstagram/src/main/java/rumad/finstagram/MainActivity.java
+++ MyFirstProject/finstagram/src/main/java/rumad/finstagram/MainActivity.java
@@ -1,52 +1,70 @@
 package rumad.finstagram;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TabHost;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    ViewPager mPager;
+    ScreenSlidePagerAdapter mPagerAdapter;
+    TabLayout mTabLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTabLayout=(TabLayout) findViewById(R.id.tab_host);
+
+        mTabLayout.addTab(mTabLayout.newTab().setText("Feed"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("Profile"));
+
+        mPager=(ViewPager)findViewById(R.id.view_pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+
+        /*TabHost.TabSpec spec=mTabHost.newTabSpec("Feed");
+
+        View v=getLayoutInflater().inflate(R.layout.fragment_home_screen,null);
+        spec.setIndicator(v);
+        spec.setContent(R.layout.fragment_home_screen);
+        mTabHost.addTab(spec);
+
+        View v2=getLayoutInflater().inflate(R.layout.cardlayout,null);
+        TabHost.TabSpec spec2=mTabHost.newTabSpec("Profile");
+        spec2.setIndicator(v2);
+        spec2.setContent(R.layout.cardlayout);
+        mTabHost.addTab(spec2);*/
 
 
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-
-        // use this setting to improve performance if you know that changes
-        // in content do not change the layout size of the RecyclerView
-        mRecyclerView.setHasFixedSize(true);
-
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        String[] CEOs = new String[]{"Musk", "Zuck", "Nadella", "Pichai", "Cook"};
-        int[] pictures=new int[]{R.drawable.elon_musk,R.drawable.mark_zuckerberg,R.drawable.satya_nadella,R.drawable.sundar_pichai,R.drawable.tim_cook};
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(CEOs,pictures);
-        mRecyclerView.setAdapter(mAdapter);
-/*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        */
     }
+
+    class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSlidePagerAdapter(FragmentManager fragmentManager){
+            super(fragmentManager);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return new HomeScreen();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+    }
+
+
 
 
 }
