30af09be919bc33c83cc456c2be7dad9fcf105fb
Hemanth <hemuchiluka3@gmail.com>
2017-11-01 20:45:26 -0400

    Week 5

--- MyFirstProject/finstagram/src/main/java/rumad/finstagram/MainActivity.java
+++ MyFirstProject/finstagram/src/main/java/rumad/finstagram/MainActivity.java
@@ -9,11 +9,18 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
-
+    static final String PIC_URL="https://pi.tedcdn.com/r/pe.tedcdn.com/images/ted/2534551796ee0a2638b462ce82e33b65091b1d42_1600x1200.jpg?w=1200";
     ViewPager mPager;
     ScreenSlidePagerAdapter mPagerAdapter;
     TabLayout mTabLayout;
@@ -23,6 +30,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar =(Toolbar)findViewById(R.id.toolbar_home);
+        ImageView profileImage=(ImageView)findViewById(R.id.image_view_profile);
+        Picasso.with(this).load(PIC_URL).resize(50,50).centerCrop().into(profileImage);
+
+
+        setSupportActionBar(toolbar);
         mTabLayout=(TabLayout) findViewById(R.id.tab_host);
 
         mTabLayout.addTab(mTabLayout.newTab().setText("Feed"));
@@ -31,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         mPager=(ViewPager)findViewById(R.id.view_pager);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
+
+
 
         /*TabHost.TabSpec spec=mTabHost.newTabSpec("Feed");
 
@@ -60,11 +75,29 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 2;
+            return 1;
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
 
-
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item_show_popup:{
+                Toast toast=Toast.makeText(this,"This is a popup",Toast.LENGTH_LONG);
+                toast.show();
+                break;
+            }
+            case R.id.item_sign_out:{
+                finish();
+                break;
+            }
+        }
+        return true;
+    }
 }
